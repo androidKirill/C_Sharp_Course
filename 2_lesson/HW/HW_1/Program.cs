a1cd3bb8aac74f2c4dd0fdeacdd82184d26701a9
celestial <m@mail.ru>
2022-11-27 09:12:48 +0300

    Task HW 2_1

---
+++ 2_lesson/HW/HW_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число
+//    и на выходе показывает вторую цифру этого числа.
+
+string NumSec(int num)
+{   
+    if (num < 1000)
+        return $"{num / 10 % 10}";
+    return "The number is not three-digit!";
+}
+
+Console.WriteLine(NumSec(int.Parse(Console.ReadLine())));
