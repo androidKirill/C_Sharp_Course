00278dc23b58a2ec2b5feab3a47d6ac05591f635
celestial <m@mail.ru>
2022-12-12 15:02:37 +0300

    Task 6_1

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while (true)
+    {
+        Console.Write("Output any number: ");
+        word = Console.ReadLine();
+
+        if ("".Join(word.Split(" ")) == "") return count;
+        else if (int.Parse(word) > 0) count += 1;        
+    }
+}
+
+Console.Write(CountNum());
