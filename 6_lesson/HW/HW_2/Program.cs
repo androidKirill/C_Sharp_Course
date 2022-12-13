95a69dbfbde6eb5e9500a05cbe7041de9d25ce1f
celestial <m@mail.ru>
2022-12-13 13:29:48 +0300

    Task 7_0

--- 6_lesson/HW/HW_2/Program.cs
+++ 6_lesson/HW/HW_2/Program.cs
@@ -9,7 +9,7 @@ void Intersection(double k1, double b1, double k2, double b2)
     double k_sub = k1 - k2;
     if (k_sub != 0)
     {
-        double x = (b2 - b1) / (k1 - k2);
+        double x = (b2 - b1) / k_sub;
         double y = k1 * x + b1;
         Console.WriteLine($"Intersection point: ({x}; {y})");
     }
