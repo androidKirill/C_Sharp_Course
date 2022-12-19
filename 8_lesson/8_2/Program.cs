b8167aeafb9cb6ae2c6d20a56e3fdca34566549a
celestial <m@mail.ru>
2022-12-19 18:03:53 +0300

    Task 8_1

--- 8_lesson/8_2/Program.cs
+++ 8_lesson/8_2/Program.cs
@@ -1,7 +1,4 @@
-﻿// 3. Задайте двумерный массив из целых чисел.
-//    Найдите среднее арифметическое элементов в каждом столбце.
-
-void Print(int[,] arr)
+﻿void Print(int[,] arr)
 {
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
@@ -9,7 +6,7 @@ void Print(int[,] arr)
     for (int i = 0; i < row_size; i++)
     {
         for (int j = 0; j < column_size; j++)
-            Console.Write($" {arr[i, j]} ");
+            Console.Write($" {arr[i, j],4} ");
         Console.WriteLine();
     }
     Console.WriteLine();
@@ -22,31 +19,45 @@ int[,] MassNums(int row, int column, int from, int to)
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
             arr[i, j] = new Random().Next(from, to);
+
     return arr;
 }
 
-void ArithmeticMean(int[,] arr)
+string Summ(int[,] array)
 {
-    int row = arr.GetLength(0);
-    int column = arr.GetLength(1);
-    double res;
+    int[] new_arr = new int[array.GetLength(0)];
 
-    for (int j = 0; j < column; j++)
-    {
-        res = 0;
-        for (int i = 0; i < row; i++) res += arr[i, j];
-        Console.Write($"{Math.Round(res / row, 2)}; ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   
+        int summa = 0;
+
+        for (int j = 0; j < array.GetLength(1); j++)
+            summa += array[i,j];
+
+        new_arr[i] = summa;
     }
+    int min_sum = new_arr[0];
+    int index = 0;
+
+    for (int i = 1; i < new_arr.Length; i++)
+    {   
+        if (new_arr[i] < min_sum) 
+        {
+            min_sum = new_arr[i];
+            index = i;
+        }
+    }
+
+    return $"String №{index + 1}";
 }
 
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
-
+Console.WriteLine("Enter range of numbers:");
 int[,] arr_1 = MassNums(row, column,
                         int.Parse(Console.ReadLine()),
                         int.Parse(Console.ReadLine()));
 Print(arr_1);
-
-ArithmeticMean(arr_1);
+Console.WriteLine(Summ(arr_1));
