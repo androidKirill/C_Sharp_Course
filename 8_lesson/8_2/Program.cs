d3fe5d77c086c0254c501ff1047467b50ce7a756
celestial <m@mail.ru>
2022-12-19 18:04:28 +0300

    Task 8_2

--- 8_lesson/8_2/Program.cs
+++ 8_lesson/8_2/Program.cs
@@ -1,4 +1,10 @@
-﻿void Print(int[,] arr)
+﻿// 2. Задайте двумерный массив. Напишите программу,
+//    которая заменяет строки на столбцы. В случае, если это невозможно,
+//    программа должна вывести сообщение для пользователя.
+
+// https://ru.wikihow.com/%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BF%D0%BE%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D1%83
+
+void Print(int[,] arr)
 {
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
@@ -6,7 +12,7 @@
     for (int i = 0; i < row_size; i++)
     {
         for (int j = 0; j < column_size; j++)
-            Console.Write($" {arr[i, j],4} ");
+            Console.Write($" {arr[i, j]} ");
         Console.WriteLine();
     }
     Console.WriteLine();
@@ -19,45 +25,35 @@ int[,] MassNums(int row, int column, int from, int to)
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
             arr[i, j] = new Random().Next(from, to);
-
     return arr;
 }
 
-string Summ(int[,] array)
+string MatrixT(int[,] arr)
 {
-    int[] new_arr = new int[array.GetLength(0)];
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    string text = "Matrix transposition is possible";
+    
+    if (row != column) return text = "Matrix transposition is not possible";
 
-    for (int i = 0; i < array.GetLength(0); i++)
-    {   
-        int summa = 0;
-
-        for (int j = 0; j < array.GetLength(1); j++)
-            summa += array[i,j];
-
-        new_arr[i] = summa;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+            (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
     }
-    int min_sum = new_arr[0];
-    int index = 0;
-
-    for (int i = 1; i < new_arr.Length; i++)
-    {   
-        if (new_arr[i] < min_sum) 
-        {
-            min_sum = new_arr[i];
-            index = i;
-        }
-    }
-
-    return $"String №{index + 1}";
+    
+    return text;
 }
 
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter range of numbers:");
+
 int[,] arr_1 = MassNums(row, column,
                         int.Parse(Console.ReadLine()),
                         int.Parse(Console.ReadLine()));
 Print(arr_1);
-Console.WriteLine(Summ(arr_1));
+
+Console.WriteLine(MatrixT(arr_1));
+Print(arr_1);
